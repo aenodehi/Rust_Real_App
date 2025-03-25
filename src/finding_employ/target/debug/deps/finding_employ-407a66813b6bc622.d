bec98032bd11b0dad0eb942e9f179e7acf06f2c8
aenodehi <aenodehi@gmail.com>
2025-03-25 19:55:20 +0330

    Added finding_employ

---
+++ src/finding_employ/target/debug/deps/finding_employ-407a66813b6bc622.d
@@ -0,0 +1,5 @@
+/home/ali/Projects/02_Rust_problems/src/finding_employ/target/debug/deps/finding_employ-407a66813b6bc622: src/main.rs
+
+/home/ali/Projects/02_Rust_problems/src/finding_employ/target/debug/deps/finding_employ-407a66813b6bc622.d: src/main.rs
+
+src/main.rs:
---
+++ src/finding_employ/target/debug/finding_employ.d
@@ -0,0 +1,1 @@
+/home/ali/Projects/02_Rust_problems/src/finding_employ/target/debug/finding_employ: /home/ali/Projects/02_Rust_problems/src/finding_employ/src/main.rs
