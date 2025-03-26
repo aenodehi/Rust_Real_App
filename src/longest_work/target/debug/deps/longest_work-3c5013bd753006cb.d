b143fcdbb644cfaabbcd31efeb44ad98ac4944fc
aenodehi <aenodehi@gmail.com>
2025-03-26 18:32:51 +0330

    Added longest_work

---
+++ src/longest_work/target/debug/deps/longest_work-3c5013bd753006cb.d
@@ -0,0 +1,5 @@
+/home/ali/Projects/02_Rust_problems/src/longest_work/target/debug/deps/longest_work-3c5013bd753006cb: src/main.rs
+
+/home/ali/Projects/02_Rust_problems/src/longest_work/target/debug/deps/longest_work-3c5013bd753006cb.d: src/main.rs
+
+src/main.rs:
---
+++ src/longest_work/target/debug/longest_work.d
@@ -0,0 +1,1 @@
+/home/ali/Projects/02_Rust_problems/src/longest_work/target/debug/longest_work: /home/ali/Projects/02_Rust_problems/src/longest_work/src/main.rs
