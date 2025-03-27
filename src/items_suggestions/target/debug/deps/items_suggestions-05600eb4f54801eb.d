321dd70375110dea0daa1533494c89a903636147
aenodehi <aenodehi@gmail.com>
2025-03-27 12:28:59 +0330

    Added items_suggestions

---
+++ src/items_suggestions/target/debug/deps/items_suggestions-05600eb4f54801eb.d
@@ -0,0 +1,5 @@
+/home/ali/Projects/02_Rust_problems/src/items_suggestions/target/debug/deps/items_suggestions-05600eb4f54801eb: src/main.rs
+
+/home/ali/Projects/02_Rust_problems/src/items_suggestions/target/debug/deps/items_suggestions-05600eb4f54801eb.d: src/main.rs
+
+src/main.rs:
---
+++ src/items_suggestions/target/debug/items_suggestions.d
@@ -0,0 +1,1 @@
+/home/ali/Projects/02_Rust_problems/src/items_suggestions/target/debug/items_suggestions: /home/ali/Projects/02_Rust_problems/src/items_suggestions/src/main.rs
